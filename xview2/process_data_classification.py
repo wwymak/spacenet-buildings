574035dc8673ee04b4c2eae6d5c6cc951e9e6210
wwymak <wwymak@gmail.com>
2019-12-09 01:03:25 +0000

    imgtuple

--- xview2/process_data_classification.py
+++ xview2/process_data_classification.py
@@ -21,7 +21,7 @@ import pandas as pd
 from tqdm import tqdm, tqdm_notebook
 import os
 from pathlib import Path
-import math
+from joblib import Parallel, delayed
 import random
 import argparse
 import logging
@@ -121,13 +121,71 @@ def process_data(data_dir, image_crops_dir, output_csv_path,):
 
     return df
 
+def parse_one_pre_post_set(label_file, img_file_pre, img_file_post, image_crops_dir_pre, image_crops_dir_post):
+    data = []
+    with open(label_file, 'r') as f:
+        label = json.load(f)
+    label_gdf = parse_json(label)
+    if label_gdf is None or len(label_gdf) == 0:
+        return
+    img_array_pre = np.array(Image.open( img_file_pre))
+    img_array_post = np.array(Image.open(img_file_post))
+    for r in label_gdf.iterrows():
+        row = r[1]
+        poly_uuid = r[0]
+        damage_type = damage_intensity_encoding[row['damage']]
+        polygon_pts = np.array(list(row.geometry_pixel.exterior.coords))
+
+        poly_img = PIL.Image.fromarray(process_img(img_array_pre, polygon_pts, 0.8))
+        poly_img.save(image_crops_dir_pre/ f"{poly_uuid}.png")
+
+        poly_img_post = PIL.Image.fromarray(process_img(img_array_post, polygon_pts, 0.8))
+        poly_img_post.save(image_crops_dir_post / f"{poly_uuid}.png")
+
+        data.append({'uuid': poly_uuid, 'img_id_pre': img_file_pre.name, 'img_id_post': img_file_post.name,
+                     'label': damage_type})
+    return data
+
+
+def process_data_pre_post_crop_pairs(data_dir, image_crops_dir_pre, image_crops_dir_post, output_csv_path,):
+    """Process Raw Data into
+        a pair of images corresponding to the pre and post of a building
+    """
+
+    images_post = [data_dir / "train" / "images"/x for x in os.listdir(data_dir / "train" / "images") if 'post_disaster' in x]
+    images_pre = [data_dir / "train" / "images"/x.name.replace('post', 'pre') for x in images_post]
+    labels = [data_dir / "train" / "labels"/(x.name.replace('png', 'json')) for x in images_post]
+
+    all_data = Parallel(n_jobs=14)(delayed(parse_one_pre_post_set)(label_file, img_file_pre,
+        img_file_post, image_crops_dir_pre, image_crops_dir_post) for
+        label_file, img_file_pre, img_file_post in  tqdm(zip(labels, images_pre, images_post)))
+
+    data_list_flat = []
+    for sublist in all_data:
+        if sublist is not None:
+            for item in sublist:
+                data_list_flat.append(item)
+    output_train_csv_path = output_csv_path/ "train_pre_post.csv"
+
+    df = pd.DataFrame(data_list_flat)
+    df.to_csv(output_train_csv_path)
+
+    return df
+
 
 def main():
+    # data_dir = Path("/media/wwymak/Storage/xView2")
+    # image_crops_dir = data_dir / "classification_crops"
+    # image_crops_dir.mkdir(exist_ok=True)
+    # process_data(data_dir,  image_crops_dir,  data_dir)
+    # logging.info("Finished Processing Data")
+
     data_dir = Path("/media/wwymak/Storage/xView2")
-    image_crops_dir = data_dir / "classification_crops"
-    image_crops_dir.mkdir(exist_ok=True)
-    process_data(data_dir,  image_crops_dir,  data_dir)
-    logging.info("Finished Processing Data")
+    image_crops_dir_post = data_dir / "classification_crops_post"
+    image_crops_dir_pre = data_dir / "classification_crops_pre"
+    image_crops_dir_post.mkdir(exist_ok=True)
+    image_crops_dir_pre.mkdir(exist_ok=True)
+    process_data_pre_post_crop_pairs(data_dir, image_crops_dir_pre, image_crops_dir_post, data_dir)
 
 
 if __name__ == '__main__':
